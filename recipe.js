625f976d18479c9cbe640f9515c169a902f3490a
collinsont2 <collinsont@stu.otc.stu.school.nz>
2023-07-18 20:17:19 +1200

    added js file

---
+++ recipe.js
@@ -0,0 +1,24 @@
+let martini = ["Gin", "Dry Vermouth", "Olive"];
+let list = document.getElementById("martiniArray");
+martini.forEach((item) => {
+    let li =
+        document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
+});
+let pinaColada = ["White Rum", "Bacardi", "Cocoanut", "pineapple", "Lime Juice"];
+let listp = document.getElementById("pinaColadaArray");
+pinaColada.forEach((item) => {
+    let li =
+        document.createElement("li");
+    li.innerText = item;
+    listp.appendChild(li);
+});
+let bloodyMary = ["Vodka", "Tomato Juice", "Lemon Juice", "Worcestershire Sause", "Tabasco"];
+let listb = document.getElementById("bloodyMaryArray");
+bloodyMary.forEach((item) => {
+    let li =
+        document.createElement("li");
+    li.innerText = item;
+    listb.appendChild(li);
+});
