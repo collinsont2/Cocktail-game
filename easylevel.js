1463b661a27529037ed5bb234bdab92851d80276
collinsont2 <collinsont@stu.otc.school.nz>
2023-07-27 13:27:51 +1200

    randomised title

--- easylevel.js
+++ easylevel.js
@@ -4,19 +4,9 @@ let pinaColada = ["White Rum", "Bacardi", "Cocoanut", "pineapple", "Lime Juice"]
 let bloodyMary = ["Vodka", "Tomato Juice", "Lemon Juice", "Worcestershire Sause", "Tabasco"];
 
 // list of drinks questions
-const drinks = ['martini', 'pinaColada', 'bloodyMary'];
+const drinklist = ['martini', 'pinaColada', 'bloodyMary'];
 
-// function that shuffles array of drinks to randomise question given
-/*function shufflearr(list){
-    const shuffledArray = list.sort(() => 0.5 - Math.random() - 0.5);
-    alert(shuffledArray[0]);
-    return shuffledArray;
-}*/
-
-
-const shuffledArray = drinks.sort(() => Math.random() - 0.5);
-alert(shuffledArray);
-
-//shufflearr(drinks);//
-
-//let (shufflearr(drinks)) = document.createElement("title");//
+// shuffle array of drinks to randomise question given
+const shuffledArray = drinklist.sort(() => Math.random() - 0.3);
+var drink = shuffledArray[0];
+document.getElementById("title").innerHTML = drink
---
+++ testings/tests.js
@@ -0,0 +1,20 @@
+// element out of list
+		/*let data = ["Ram", "Shyam",
+					"Sita", "Gita"];
+		let list =
+			document.getElementById("myList");
+
+		data.forEach((item) => {
+			let li =
+				document.createElement("li");
+			li.innerText = item;
+			list.appendChild(li);
+		});*/
+
+		//shuffle type 1
+		/*const arr = ['dog', 'cat', 'dragon','slingacademy.com', 'banana'];
+
+		const shuffledArr = arr.sort(() => Math.random() - 0.5);
+		alert(shuffledArr);*/
+
+		//shuffle type 2
